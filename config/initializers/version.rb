45794f97fb11a2d459af389d61f40c489cf1fc15
IrinaKirilina <95235793+IrinaKirilina@users.noreply.github.com>
2022-09-05 16:21:30 +0500

    Revert "Initial commit"

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,7 +1,9 @@
 #  при вызове TestGuru.current_version возвращаетсяся код текущей версии приложения.
 
-module TestGuru
-  class Application
-    CURRENT_VERSION = '0.1.0'
-  end
+class TestGuru
+	class << self
+		def current_version 
+			"0.1.0"
+		end
+	end
 end
